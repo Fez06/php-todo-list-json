a3b33a832cac662fb4320d9dc5ec27028c35a89a
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-04-12 16:18:40 +0200

    struttura base

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP ToDo List JSON</title>
+    <!-- vuejs -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+
 </head>
 <body>
-    
+    <div id="app">
+        <h1>To Do List</h1>
+    </div>
+    <script src="main.js"></script>
 </body>
 </html>
