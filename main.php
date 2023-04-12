e9927f91a4b9437c237475fc6a7e699d516ccc69
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-04-12 18:53:50 +0200

    json letto da php e stampato

--- main.php
+++ main.php
@@ -4,5 +4,14 @@ $todoList = file_get_contents(__DIR__.'/todolist.json');
 
 $todoList = json_decode($todoList, true);
 
-var_dump($todoList);
+//$todoList[0]['text'] = 'merdaaa';
+
+//var_dump($todoList);
+
+//$todoList = json_encode($todoList);
+
+header('content-Type: application/json');
+echo json_encode($todoList);
+
+
 
